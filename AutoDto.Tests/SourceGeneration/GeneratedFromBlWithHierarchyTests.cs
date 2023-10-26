ca7c74133dc7f018d8c143462ccd27d5c36cd2bb
Ohorodnikov <ogo.ogo50@gmail.com>
2023-10-26 13:06:27 +0300

    Refactor GeneratedFromBlWithHierarchyTests

--- AutoDto.Tests/SourceGeneration/GeneratedFromBlWithHierarchyTests.cs
+++ AutoDto.Tests/SourceGeneration/GeneratedFromBlWithHierarchyTests.cs
@@ -1,5 +1,10 @@
-﻿using AutoDto.Tests.SourceGeneration.Models.HierarchyTestModels;
+﻿using AutoDto.Tests.SourceGeneration.Models;
+using AutoDto.Tests.SourceGeneration.Models.HierarchyTestModels;
 using AutoDto.Tests.TestHelpers;
+using AutoDto.Tests.TestHelpers.CodeBuilder.Builders;
+using AutoDto.Tests.TestHelpers.CodeBuilder.Elements;
+using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
 using static AutoDto.Tests.TestHelpers.DtoCodeCreator;
 using static AutoDto.Tests.TestHelpers.SyntaxChecker;
 
@@ -7,31 +12,291 @@ namespace AutoDto.Tests.SourceGeneration;
 
 public class GeneratedFromBlWithHierarchyTests : BaseUnitTest
 {
-    private static string _dtoName = "MyDto";
-
-    private DtoData NewData(Type blType)
+    [Fact]
+    public void BlWithSimpleBaseTest()
     {
-        return new DtoData(blType, Setup.RelationStrategy.None, _dtoName);
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+
+        var expectedPropsInDto = new[] 
+        { 
+            idProp, 
+            nameProp 
+        };
+
+        var baseBl = 
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace) 
+            .AddMember(idProp)
+            .Build();
+
+        var blClass =
+            new ClassBuilder("SimpleBl")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMember(nameProp)
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, blClass });
     }
 
-    [Theory]
-    [InlineData(typeof(SimpleBl))]
-    [InlineData(typeof(BaseGenericBl<string>))]
-    [InlineData(typeof(GenericBl))]
-    [InlineData(typeof(BlWithNonPublicProp))]
-    [InlineData(typeof(BlInheritedFromBlWithNonPublicProp))]
-    [InlineData(typeof(BlWithMembers))]
-    [InlineData(typeof(BlInheritedFromBlWithMembers))]
-    public void BlWithHierarchy_IncludePublicProps_Test(Type blType)
+    [Fact]
+    public void BlIsGenericTypeDefinitionTest()
     {
-        var dtoData = NewData(blType);
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
 
-        var compilation = RunForDtos(dtoData);
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp
+        };
 
-        var generated = SyntaxChecker.FindClassByName(compilation, dtoData.DtoName);
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
 
-        var expected = dtoData.Type.GetPublicInstProperties().Select(x => new PropertyDescriptor(x));
+        var blClass =
+            new ClassBuilder("BaseGenericBl<T>")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMember(new PropertyBuilder("Name", "T").Build())
+            .Build();
 
-        SyntaxChecker.TestOneClassDeclaration(generated, expected);
+        var dtoClass =
+            new DtoClassBuilder("MyDto", DtoClassBuilder.DtoAttributeType.DtoFrom, "BaseGenericBl<string>", blClass.Namespace)
+            .SetNamespace(DtoNamespace)
+            .Build();
+
+        RunWithAssert(new[] { baseBl, blClass, dtoClass }, DoAssert);
+
+        void DoAssert(Compilation compilation, ImmutableArray<Diagnostic> msgs)
+        {
+            Assert.Empty(msgs);
+
+            var generatedClass = SyntaxChecker.FindAllClassDeclarationsByName(compilation, dtoClass.Name)
+                    .Skip(1) //skip declaration to get only generated
+                    .Single();
+
+            SyntaxChecker.TestOneClassDeclaration(generatedClass, Member2PropDescriptor(expectedPropsInDto));
+        }
+    }
+
+    [Fact]
+    public void BlHasGenericBaseTest()
+    {
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+        var nameProp2 = new PropertyBuilder("Name2", typeof(string)).Build();
+
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp,
+            nameProp2,
+        };
+
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
+
+        var baseBl2 =
+            new ClassBuilder("BaseGenericBl<T>")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMember(new PropertyBuilder("Name", "T").Build())
+            .Build();
+
+        var blClass =
+            new ClassBuilder("GenericBl")
+            .SetNamespace(BlNamespace)
+            .AddBase("BaseGenericBl<string>")
+            .AddMember(nameProp2)
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, baseBl2, blClass });
+    }
+
+    [Fact]
+    public void BlHasNonPublicPropsTest()
+    {
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp,
+        };
+
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
+
+        var blClass =
+            new ClassBuilder("BlWithNonPublicProp")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMember(nameProp)
+            .AddMember(new PropertyBuilder("PrivateProp", typeof(string)).SetAccessor(Visibility.Private).Build())
+            .AddMember(new PropertyBuilder("ProtectedProp", typeof(string)).SetAccessor(Visibility.Protected).Build())
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, blClass });
+    }
+
+    [Fact]
+    public void BlHasBaseWithNonPublicPropsTest()
+    {
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+        var nameProp2 = new PropertyBuilder("Name2", typeof(string)).Build();
+
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp,
+            nameProp2,
+        };
+
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
+
+        var baseBl2 =
+            new ClassBuilder("BlWithNonPublicProp")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMember(nameProp)
+            .AddMember(new PropertyBuilder("PrivateProp", typeof(string)).SetAccessor(Visibility.Private).Build())
+            .AddMember(new PropertyBuilder("ProtectedProp", typeof(string)).SetAccessor(Visibility.Protected).Build())
+            .Build();
+
+        var blClass =
+            new ClassBuilder("BlInheritedFromBlWithNonPublicProp")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl2)
+            .AddMember(nameProp2)
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, baseBl2, blClass });
+    }
+
+    [Fact]
+    public void BlHasMembersNotPropsTest()
+    {
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp,
+        };
+
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
+
+        var blClass =
+            new ClassBuilder("BlWithMembers")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMembers(new Member[]
+            {
+                nameProp,
+
+                new MethodBuilder("PrivateMethod", typeof(void)).SetAccessor(Visibility.Private).Build(),
+                new MethodBuilder("ProtectedMethod", typeof(void)).SetAccessor(Visibility.Protected).Build(),
+                new MethodBuilder("PublicMethod", typeof(void)).SetAccessor(Visibility.Public).Build(),
+
+                new FieldBuilder("PrivateField", typeof(string)).SetAccessor(Visibility.Private).Build(),
+                new FieldBuilder("ProtectedField", typeof(string)).SetAccessor(Visibility.Protected).Build(),
+                new FieldBuilder("PublicField", typeof(string)).SetAccessor(Visibility.Public).Build(),
+            })
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, blClass });
+    }
+
+    [Fact]
+    public void BlHasBaseWithMembersNotPropsTest()
+    {
+        var idProp = CommonProperties.Id_Int;
+        var nameProp = new PropertyBuilder("Name", typeof(string)).Build();
+        var nameProp2 = new PropertyBuilder("Name2", typeof(string)).Build();
+
+        var expectedPropsInDto = new[]
+        {
+            idProp,
+            nameProp,
+            nameProp2,
+        };
+
+        var baseBl =
+            new ClassBuilder("BaseBl")
+            .SetNamespace(BlNamespace)
+            .AddMember(idProp)
+            .Build();
+
+        var baseBl2 =
+            new ClassBuilder("BlWithMembers")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl)
+            .AddMembers(new Member[]
+            {
+                nameProp,
+
+                new MethodBuilder("PrivateMethod", typeof(void)).SetAccessor(Visibility.Private).Build(),
+                new MethodBuilder("ProtectedMethod", typeof(void)).SetAccessor(Visibility.Protected).Build(),
+                new MethodBuilder("PublicMethod", typeof(void)).SetAccessor(Visibility.Public).Build(),
+
+                new FieldBuilder("PrivateField", typeof(string)).SetAccessor(Visibility.Private).Build(),
+                new FieldBuilder("ProtectedField", typeof(string)).SetAccessor(Visibility.Protected).Build(),
+                new FieldBuilder("PublicField", typeof(string)).SetAccessor(Visibility.Public).Build(),
+            })
+            .Build();
+
+        var blClass =
+            new ClassBuilder("BlInheritedFromBlWithMembers")
+            .SetNamespace(BlNamespace)
+            .AddBase(baseBl2)
+            .AddMember(nameProp2)
+            .Build();
+
+        BuildDtoFromLastBlAndCheckAllProps(expectedPropsInDto, new[] { baseBl, baseBl2, blClass });
+    }
+
+    private void BuildDtoFromLastBlAndCheckAllProps(PropertyMember[] expectedPropsInDto, ClassElement[] classes)
+    {
+        var blForDto = classes.Last();
+
+        var dtoClass =
+            new DtoClassBuilder("MyDto", DtoClassBuilder.DtoAttributeType.DtoFrom, blForDto)
+            .SetNamespace(DtoNamespace)
+            .Build();
+
+        RunWithAssert(classes, DoAssert);
+
+        void DoAssert(Compilation compilation, ImmutableArray<Diagnostic> msgs)
+        {
+            Assert.Empty(msgs);
+
+            var generatedClass = SyntaxChecker.FindAllClassDeclarationsByName(compilation, dtoClass.Name)
+                    .Skip(1) //skip declaration to get only generated
+                    .Single();
+
+            SyntaxChecker.TestOneClassDeclaration(generatedClass, Member2PropDescriptor(expectedPropsInDto));
+        }
     }
 }
